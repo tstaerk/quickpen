274d8c8a61ead4a750397feb0c83463b7f37cde6
Thorsten Staerk <thorsten@staerk.de>
2011-09-03 08:46:23 +0200

    adding German translation

---
+++ quickpen_de.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>QuickPen</source>
+        <translation>QuickPen</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="25"/>
+        <source>Previous page</source>
+        <translation>Vorige Seite</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="36"/>
+        <source>Next page</source>
+        <translation>Naechste Seite</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="50"/>
+        <source>More</source>
+        <translation>Mehr</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="73"/>
+        <source>save</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MoreForm</name>
+    <message>
+        <location filename="moreform.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
